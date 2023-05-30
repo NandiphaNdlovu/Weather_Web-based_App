21e526db3c7e2839b00bfa4b1d06ea2ead7dd8fb
Solum <haydaust@gmail.com>
2023-05-30 10:18:00 +0200

    Handling null and undefined inputs

--- server.js
+++ server.js
@@ -5,19 +5,22 @@ const app = express();
 const port = 3000; // Choose an available port number
 
 app.use(express.static(path.join(__dirname, "views")));
+app.use(express.static(path.join(__dirname, "css")));
+
 
 
 app.get("/weather", async (req, res) => {
   const apiKey = "5a9d2c05e78649dba29133051232905";
-  const location = req.query.location;
+  var location = req.query.location;
   const fetch = await import("node-fetch");
-  const apiUrl = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${location}&aqi=no`;
+  var apiUrl = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${location}&aqi=no`;
 
   try {
     const response = await fetch.default(apiUrl);
     const responseBody = await response.text();
 
     console.log("Raw Response Body:", responseBody);
+    console.log("Location input:", location,":");
 
     let jsonData;
     try {
@@ -37,7 +40,7 @@ app.get("/weather", async (req, res) => {
 });
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "views", "weather.html"));
+  res.sendFile(path.join(__dirname, "weather.html"));
 });
 
 app.listen(port, () => {
